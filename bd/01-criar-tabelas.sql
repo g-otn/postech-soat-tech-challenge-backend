2202b6e75f69f86900023bdfd5bc4b45e966fe7b
g-otn <44736064+g-otn@users.noreply.github.com>
2023-06-20 18:48:57 -0300

    fix: dummy sql script and config

--- bd/01-criar-tabelas.sql
+++ bd/01-criar-tabelas.sql
@@ -1,6 +1,5 @@
-create table public._dummy3
-(
-    id   bigint not null
-        primary key,
-    aaaa varchar(255)
-);
+create table
+    public._dummy (
+        id bigint not null primary key,
+        aaaa varchar(255)
+    );
