96705b3b2c9bc5497a21997d8ef8f4f38cd155aa
Marcelo Vilas Boas Correa Filho <marcelovbcfilho@gmail.com>
2023-06-26 22:31:35 -0300

    feat: adding new order queue query

--- techchallenge/src/main/java/br/com/grupo63/techchallenge/core/application/repository/IOrderRepository.java
+++ techchallenge/src/main/java/br/com/grupo63/techchallenge/core/application/repository/IOrderRepository.java
@@ -1,6 +1,21 @@
 package br.com.grupo63.techchallenge.core.application.repository;
 
 import br.com.grupo63.techchallenge.core.domain.entity.Order;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IOrderRepository extends IRepository<Order> {
+
+    @Query("SELECT " +
+            "   order " +
+            "FROM " +
+            "   Order order " +
+            "WHERE " +
+            "   order.status != Order.OrderStatus.DONE " +
+            "   AND order.deleted = false " +
+            "ORDER BY " +
+            "   order.creationDate")
+    List<Order> findByStatusDoneAndDeletedFalseOrderByCreationDate();
+
 }
