432e5315e84a37e4298235d36364b703b5dc992c
Marcelo Vilas Boas Correa Filho <marcelovbcfilho@gmail.com>
2023-06-26 22:22:28 -0300

    feat: adding new order repository

---
+++ techchallenge/src/main/java/br/com/grupo63/techchallenge/core/application/repository/IOrderRepository.java
@@ -0,0 +1,6 @@
+package br.com.grupo63.techchallenge.core.application.repository;
+
+import br.com.grupo63.techchallenge.core.domain.entity.Order;
+
+public interface IOrderRepository extends IRepository<Order> {
+}
