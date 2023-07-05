0e0cb03ba9eb4d6c989f2cbed70ed1e1f79e42dd
Marcelo Vilas Boas Correa Filho <marcelovbcfilho@gmail.com>
2023-07-05 19:48:26 -0300

    feat: added find by category

--- techchallenge/src/main/java/br/com/grupo63/techchallenge/core/application/repository/IProductRepository.java
+++ techchallenge/src/main/java/br/com/grupo63/techchallenge/core/application/repository/IProductRepository.java
@@ -2,12 +2,15 @@ package br.com.grupo63.techchallenge.core.application.repository;
 
 import br.com.grupo63.techchallenge.core.domain.entity.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IProductRepository extends IRepository<Product> {
 
     Product saveAndFlush(Product product);
 
-    Optional<Product> findById(Long id);
+    Optional<Product> findByIdAndDeletedFalse(Long id);
+
+    List<Product> findByDeletedFalseAndCategory_Id(Long categoryId);
 
 }
